22fd42fb4f066209569a6820cfa21db02d69418d
Asutosh Roy <royasutosh7@gmail.com>
2022-04-27 00:42:58 +0530

    first commit

---
+++ wp-content/themes/Blog/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+// Setup
+
+
+// Includes
+
+include(get_theme_file_path('/includes/font/enqueue.php'));
+
+// Hooks
+
+add_action( 'wp_enqueue_scripts', 'ju_enqueue' );
+
+// Shortcodes
+
+?>
---
+++ wp-content/themes/Blog/includes/font/enqueue.php
@@ -0,0 +1,37 @@
+<?php
+
+
+
+ function ju_enqueue()
+{
+    $uri = get_theme_file_uri();
+
+
+    /* finding all style sheet*/
+     wp_register_style('ju_google_fonts','https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Raleway:300,400,500,600,700|Crete+Round:400i'); 
+     wp_register_style('ju_bootstrap', $uri.'/assets/css/bootstrap.css'); 
+     wp_register_style('ju_style', $uri.'/assets/css/style.css'); 
+     wp_register_style('ju_dark', $uri.'/assets/css/dark.css'); 
+     wp_register_style('ju_font-icons', $uri.'/assets/css/font-icons.css'); 
+     wp_register_style('ju_animate', $uri.'/assets/css/animate.css'); 
+     wp_register_style('ju_magnific-popup', $uri.'/assets/css/magnific-popup.css'); 
+     wp_register_style('ju_responsive', $uri.'/assets/css/responsive.css'); 
+     wp_register_style('ju_custom', $uri.'/assets/css/custom.css'); 
+
+     /* ask to give output all style sheet*/
+     wp_enqueue_style('ju_google_fonts','https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Raleway:300,400,500,600,700|Crete+Round:400i');
+     wp_enqueue_style('ju_bootstrap', $uri.'/assets/css/bootstrap.css'); 
+     wp_enqueue_style('ju_style', $uri.'/assets/css/style.css'); 
+     wp_enqueue_style('ju_dark', $uri.'/assets/css/dark.css'); 
+     wp_enqueue_style('ju_font-icons', $uri.'/assets/css/font-icons.css'); 
+     wp_enqueue_style('ju_animate', $uri.'/assets/css/animate.css'); 
+     wp_enqueue_style('ju_magnific-popup', $uri.'/assets/css/magnific-popup.css'); 
+     wp_enqueue_style('ju_responsive', $uri.'/assets/css/responsive.css'); 
+     wp_enqueue_style('ju_custom', $uri.'/assets/css/custom.css'); 
+        
+ /* finding all js*/
+     wp_register_script('ju_plugins', $uri.'/assets/js/plugin.js');
+    
+}
+
+?>
